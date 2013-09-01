7200a3497e834e4a644e590f63ee9c7e7c8be395
Manuel Quezada <manuel.quezada@kaust.edu.sa>
2013-09-02 01:01:55 +0300

    add code to plot numerical experiments

---
+++ numerical_experiments/2D-propagation/plotting_slices/c-dispersion/layered/plotting_slices.m
@@ -0,0 +1,34 @@
+clear; clc; close all
+figure(1); clf; hold on
+set(1, 'Position', [50 50 800 300])
+
+%% plot FV solution
+FV_x_slice=load('c-dispersion_layered_xslice.txt');
+FV_y_slice=load('c-dispersion_layered_yslice.txt');
+xFV = FV_x_slice(:,1);
+pxFV = FV_x_slice(:,2);
+yFV = FV_y_slice(:,1);
+pyFV = FV_y_slice(:,2);
+plot(xFV,pxFV,'-b','linewidth',2)
+plot(yFV,pyFV,'-r','linewidth',2)
+
+%% plot homogenized solution
+T=65; nt=T/0.5+1; %dt=0.5
+load('c-dispersion_layered.mat')
+pp=squeeze(U(nt,:,:));
+x=squeeze(xx(end/2,mx/2+1:mx))'-100+xFV(1); y=squeeze(yy(my/2+1:my,end/2))'-100+yFV(1);
+px=pp(end/2,mx/2+1:mx); py=pp(my/2+1:my,end/2);
+xi=linspace(x(1),x(end),1000); pxi=spline(x,px,xi);
+yi=linspace(y(1),y(end),1000); pyi=spline(y,py,yi);
+plot(xi,pxi,'--k','linewidth',2)
+plot(yi,pyi,'-.k','linewidth',2)
+
+%% formatting
+xlim([40, 90])
+ylim([-0.25,0.25])
+title(['t=' num2str((nt-1)*0.5)],'fontsize',20);
+xlabel('x, y','fontsize',20)
+leg=legend('FV x-slice', 'FV y-slice','Homogenized x-slice','Homogenized y-slice','location','northwest');
+set(leg,'fontsize',20)
+set(gca,'fontsize',20)
+
---
+++ numerical_experiments/2D-propagation/plotting_slices/z-dispersion/plotting_slices.m
@@ -0,0 +1,35 @@
+clear; clc; close all
+figure(1); clf; hold on
+set(1, 'Position', [50 50 800 300])
+
+%% plot FV solution
+FV_x_slice=load('z-dispersion_xslice.txt');
+FV_y_slice=load('z-dispersion_yslice.txt');
+xFV = FV_x_slice(:,1);
+pxFV = FV_x_slice(:,2);
+yFV = FV_y_slice(:,1);
+pyFV = FV_y_slice(:,2);
+plot(xFV,pxFV,'-b','linewidth',2)
+plot(yFV,pyFV,'-r','linewidth',2)
+
+%% plot homogenized solution
+T=95; nt=T/0.5+1; %dt=0.5
+load('z-dispersion.mat')
+pp=squeeze(U(nt,:,:));
+x=squeeze(xx(end/2,mx/2+1:mx))'-100+xFV(1); y=squeeze(yy(my/2+1:my,end/2))'-100+yFV(1);
+px=pp(end/2,mx/2+1:mx); py=pp(my/2+1:my,end/2);
+xi=linspace(x(1),x(end),1000); pxi=spline(x,px,xi);
+yi=linspace(y(1),y(end),1000); pyi=spline(y,py,yi);
+plot(xi,pxi,'--k','linewidth',2)
+plot(yi,pyi,'-.k','linewidth',2)
+
+%% formatting
+xlim([40, 100])
+title(['t=' num2str((nt-1)*0.5)],'fontsize',20);
+xlabel('x, y','fontsize',20)
+leg=legend('FV x-slice', 'FV y-slice','Homogenized x-slice','Homogenized y-slice','location','northwest');
+set(leg,'fontsize',20)
+set(gca,'fontsize',20)
+
+
+
