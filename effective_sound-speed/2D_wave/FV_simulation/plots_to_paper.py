8afdee12aba079265dde90e4eec79f14a861f5bc
Manuel Quezada <mquezada@kl-12351.kaust.edu.sa>
2014-05-14 16:21:54 +0300

    changes to generation of diffraction plot and leading order vs FV plot

--- effective_sound-speed/2D_wave/FV_simulation/plots_to_paper.py
+++ effective_sound-speed/2D_wave/FV_simulation/plots_to_paper.py
@@ -8,6 +8,7 @@ from matplotlib import rc
 #rc('text', usetex=True)
 import numpy as np
 import os
+import scipy.io
 
 def plot_p(frame):
     sol=Solution(frame,file_format='petsc',read_aux=False,path='./_output/_p/',file_prefix='claw_p')
@@ -18,12 +19,13 @@ def plot_p(frame):
     yy,xx = np.meshgrid(y,x)
 
     p=sol.state.q[0,:,:]
-    pl.figure(figsize=(8, 3))
-    pl.title("t= "+str(sol.state.t),fontsize=20)
+    fig = pl.figure(figsize=(8, 3))
+    #pl.title("t= "+str(sol.state.t),fontsize=20)
     pl.xticks(size=20); pl.yticks(size=20)
     #pl.xlabel('x',fontsize=20); pl.ylabel('y',fontsize=20)
     #pl.pcolormesh(xx,yy,p_subxy,cmap=cm.OrRd)
     pl.pcolormesh(xx,yy,p,cmap='RdBu_r')
+    pl.autoscale(tight=True)
     cb = pl.colorbar(ticks=[0.5,1,1.5,2]);
     
     #pl.clim(ticks=[0.5,1,1.5,2])
@@ -31,7 +33,33 @@ def plot_p(frame):
     pl.yticks(fontsize=20); pl.axes(imaxes)
     #pl.xticks(fontsize=20); pl.axes(imaxes)
     pl.axis('equal')
+    pl.axis('tight')
     pl.savefig('./_plots_to_paper/sound-speed_FV_t'+str(frame)+'_pcolor.png')
+    pl.close()
+
+def plot_p_leading_order(frame):
+    mat = scipy.io.loadmat('sound-speed_2D-wave.mat')
+    T=5; nt=T/0.5
+    pp=mat['U'][nt,:,:]
+    xx=mat['xx']
+    yy=mat['yy']
+
+    pl.figure(figsize=(8, 3))
+    #pl.title("t= "+str(sol.state.t),fontsize=20)
+    pl.xticks(size=20); pl.yticks(size=20)
+    #pl.xlabel('x',fontsize=20); pl.ylabel('y',fontsize=20)
+    #pl.pcolormesh(xx,yy,p_subxy,cmap=cm.OrRd)
+    pl.pcolormesh(xx,yy,pp,cmap='RdBu_r')
+    pl.autoscale(tight=True)
+    cb = pl.colorbar(ticks=[0.5,1,1.5,2]);
+    
+    #pl.clim(ticks=[0.5,1,1.5,2])
+    imaxes = pl.gca(); pl.axes(cb.ax)
+    pl.yticks(fontsize=20); pl.axes(imaxes)
+    #pl.xticks(fontsize=20); pl.axes(imaxes)
+    pl.axis('equal')
+    pl.axis('tight')
+    pl.savefig('./_plots_to_paper/sound-speed_LO_t'+str(frame)+'_pcolor.png')
     pl.close()
 
 if __name__== "__main__":
@@ -41,3 +69,4 @@ if __name__== "__main__":
     print('**********************')
     print('Plotting p ...')
     plot_p(frame=5)
+    plot_p_leading_order(frame=5)
